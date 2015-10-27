8df1c598d781e766bc2eca3589bd29459af59077
CorrieASmith <corriesmith87@gmail.com>
2015-10-27 09:15:38 -0700

    seed database with would you rather questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+class Question < ActiveRecord::Base
+
+  questions = Question.create([{ name: "Would you rather...", id: 100 }])
+  questions = Question.create([{ name: "Would you rather date...", id: 200 }])
+  questions = Question.create([{ name: "Would you rather have no one show up to...", id: 300 }])
+  questions = Question.create([{ name: "Would you rather be able to...", id: 400 }])
+  questions = Question.create([{ name: "Would you rather know...", id: 500 }])
+
+end
+
+class Condition < ActiveRecord::Base
+
+  conditions = Condition.create([{ description: "Try and climb Mount Everest in flip flops", question_id: 100 }])
+  conditions = Condition.create([{ description: "Cut off your big toe", question_id: 100 }])
+
+  conditions = Condition.create([{ description: "A celebrity of your choice", question_id: 200 }])
+  conditions = Condition.create([{ description: "Your real life crush", question_id: 200 }])
+
+  conditions = Condition.create([{ description: "Your wedding", question_id: 300 }])
+  conditions = Condition.create([{ description: "Your funeral", question_id: 300 }])
+
+  conditions = Condition.create([{ description: "Fly", question_id: 400 }])
+  conditions = Condition.create([{ description: "Read minds", question_id: 400 }])
+
+  conditions = Condition.create([{ description: "The date of your death", question_id: 500 }])
+  conditions = Condition.create([{ description: "The cause of your death", question_id: 500 }])
+
+end
